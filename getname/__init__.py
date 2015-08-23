183888ce27a58a53be5e565ee7d7f7f4320a6a7a
lord63 <lord63.j@gmail.com>
2015-08-23 10:13:01 +0800

    Releave v0.1.1

--- getname/__init__.py
+++ getname/__init__.py
@@ -14,7 +14,7 @@
 from getname.main import random_name
 
 __title__ = "getname"
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = "lord63"
 __license__ = "MIT"
 __copyright__ = "Copyright 2015 lord63"
